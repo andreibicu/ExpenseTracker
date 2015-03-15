235c0f49181d9536462bfa1e907b8567261ed0fd
jicking <jdbebiro@gmail.com>
2015-03-16 07:14:34 +0800

    added test for repo factory

---
+++ DataApp.Core.Test/IRepositoryFactoryTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataApp.Core.Abstracts;
+using DataApp.Core.DAL;
+
+namespace DataApp.Core.Test
+{
+    [TestClass]
+    public class IRepositoryFactoryTest
+    {
+        IRepositoryFactory target = null;
+        DataAppContext dbContext = null;
+
+        [TestMethod]
+        public void CreateRepository()
+        {
+            //arrage
+            DataApp.Core.Abstracts.IRepository<DataApp.Core.Models.User> repo = null;
+            this.dbContext = new DAL.DataAppContext();
+            this.target = new DataApp.Core.Factories.RepoFactory();
+
+            //act
+            repo = this.target.CreateRepository<DataApp.Core.Models.User>(this.dbContext);
+
+            //assert
+            Assert.IsNotNull(repo);
+        }
+    }
+}
