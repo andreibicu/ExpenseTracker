320a6a9968933b3790482dca3df553b5f710b8ba
jicking <jdbebiro@gmail.com>
2015-03-16 07:11:58 +0800

    Improved Tests

--- DataApp.Core.Test/IRepositoryTest.cs
+++ DataApp.Core.Test/IRepositoryTest.cs
@@ -11,55 +11,60 @@ namespace DataApp.Core.Test
     {
         IRepositoryFactory factory = null;
         DataAppContext dbContext = null;
-        IRepository<Project> target = null;
+        /// <summary>
+        /// Target of our test.
+        /// </summary>
+        IRepository<User> repository = null;
+        /// <summary>
+        /// Object to be used by our target as an argument.
+        /// </summary>
+        User entity = null;
 
+        /// <summary>
+        /// Encapslates Arrange process segment in our tests.
+        /// </summary>
         [TestInitialize]
         public void Init()
         {
             this.dbContext = new DAL.DataAppContext();
             this.factory = new DataApp.Core.Factories.RepoFactory();
-            this.target = this.factory.CreateRepository<Project>(this.dbContext);
+            this.repository = this.factory.CreateRepository<User>(this.dbContext);
+            this.entity = new User();
+            entity.Username = "New";
+            entity.Password = "New";
         }
 
         [TestMethod]
         public void Add()
         {
             //arrange
-            var project = new Project();
-            project.Name = "test";
-
             //act
-
+            //done on Init()
             //assert
-            Assert.IsTrue(this.target.Add(project));
+            Assert.IsTrue(this.repository.Add(this.entity));
         }
 
         [TestMethod]
         public void Update()
         {
             //arrange
-            var project = new Project();
-
             //act
-            this.target.Add(project);
-            project.Name = "Update";
+            this.repository.Add(this.entity);
+            this.entity.Username = "Update";
+            this.entity.Password = "Update";
 
             //assert
-            Assert.IsTrue(this.target.Update(project));
+            Assert.IsTrue(this.repository.Update(this.entity));
         }
 
         [TestMethod]
         public void Delete()
         {
             //arrange
-            var project = new Project();
-            project.Name = "To Be Deleted";
-
             //act
-            this.target.Add(project);
-
+            this.repository.Add(this.entity);
             //assert
-            Assert.IsTrue(this.target.Update(project));
+            Assert.IsTrue(this.repository.Update(this.entity));
         }
 
         [TestMethod]
@@ -67,10 +72,9 @@ namespace DataApp.Core.Test
         {
             //arrange
             //act
-            var project = this.target.Get(p => p.ID > 0);
-
+            var user = this.repository.Get(p => p.ID > 0);
             //assert
-            Assert.IsNotNull(project);
+            Assert.IsNotNull(user);
         }
 
         [TestMethod]
@@ -78,10 +82,9 @@ namespace DataApp.Core.Test
         {
             //arrange
             //act
-            var projects = this.target.GetAll();
-
+            var user = this.repository.GetAll();
             //assert
-            Assert.IsNotNull(projects);
+            Assert.IsNotNull(user);
         }
     }
 }
--- DataAppLib/DAL/DataAppContext.cs
+++ DataAppLib/DAL/DataAppContext.cs
@@ -16,56 +16,14 @@ namespace DataApp.Core.DAL
         // If you wish to target a different database and/or database provider, modify the 'DataAppContext' 
         // connection string in the application configuration file.
         public DataAppContext()
-            : base("name=DataAppContext")
+            : base("DataAppContext")
         {
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<DataAppContext, Configuration>("name=DataAppContext"));
         }
 
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
-        public virtual DbSet<Voucher> Vouchers { get; set; }
-        public virtual DbSet<Check> Checks { get; set; }
-        public virtual DbSet<CheckTransaction> CheckTransactions { get; set; }
-        public virtual DbSet<Account> Accounts { get; set; }
-        public virtual DbSet<Expense> Expenses { get; set; }
-        public virtual DbSet<ExpenseItem> ExpenseItems { get; set; }
-        public virtual DbSet<Project> Projects { get; set; }
-
-
-
         public virtual DbSet<User> Users { get; set; }
-
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-
-            base.OnModelCreating(modelBuilder);
-        }
-    }
-
-    internal sealed class Configuration : DbMigrationsConfiguration<DataAppContext>
-    {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = true;
-        }
-
-        protected override void Seed(DataAppContext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
     }
 
 }
