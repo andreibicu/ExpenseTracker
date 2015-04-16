7347ca36d153e18b72f9a1fe74791620d25b0604
jicking <jdbebiro@gmail.com>
2015-04-16 19:16:13 +0800

    disabled test

--- DataApp.Core.Test/IRepositoryFactoryTest.cs
+++ DataApp.Core.Test/IRepositoryFactoryTest.cs
@@ -1,28 +1,28 @@
-﻿using DataApp.Core.Abstracts;
-using DataApp.Core.DAL;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using DataApp.Core.Abstracts;
+//using DataApp.Core.DAL;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace DataApp.Core.Test
-{
-    [TestClass]
-    public class IRepositoryFactoryTest
-    {
-        IRepositoryFactory target = null;
-        DataAppContext dbContext = null;
+//namespace DataApp.Core.Test
+//{
+//    [TestClass]
+//    public class IRepositoryFactoryTest
+//    {
+//        IRepositoryFactory target = null;
+//        DataAppContext dbContext = null;
 
-        [TestMethod]
-        public void CreateRepository()
-        {
-            //arrage
-            DataApp.Core.Abstracts.IRepository<DataApp.Core.Models.User> repo = null;
-            this.dbContext = new DAL.DataAppContext();
-            this.target = new DataApp.Core.Factories.RepoFactory();
+//        [TestMethod]
+//        public void CreateRepository()
+//        {
+//            //arrage
+//            DataApp.Core.Abstracts.IRepository<DataApp.Core.Models.User> repo = null;
+//            this.dbContext = new DAL.DataAppContext();
+//            this.target = new DataApp.Core.Factories.RepoFactory();
 
-            //act
-            repo = this.target.CreateRepository<DataApp.Core.Models.User>(this.dbContext);
+//            //act
+//            repo = this.target.CreateRepository<DataApp.Core.Models.User>(this.dbContext);
 
-            //assert
-            Assert.IsNotNull(repo);
-        }
-    }
-}
+//            //assert
+//            Assert.IsNotNull(repo);
+//        }
+//    }
+//}
--- DataApp.Core.Test/IRepositoryTest.cs
+++ DataApp.Core.Test/IRepositoryTest.cs
@@ -1,121 +1,121 @@
-﻿using DataApp.Core.Abstracts;
-using DataApp.Core.DAL;
-using DataApp.Core.Models;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using DataApp.Core.Abstracts;
+//using DataApp.Core.DAL;
+//using DataApp.Core.Models;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace DataApp.Core.Test
-{
-    [TestClass]
-    public class IRepositoryTest
-    {
-        IRepositoryFactory factory = null;
-        DataAppContext dbContext = null;
-        /// <summary>
-        /// Target of our test.
-        /// </summary>
-        IRepository<User> repository = null;
-        /// <summary>
-        /// Object to be used by our target as an argument.
-        /// </summary>
-        User entity = null;
+//namespace DataApp.Core.Test
+//{
+//    [TestClass]
+//    public class IRepositoryTest
+//    {
+//        IRepositoryFactory factory = null;
+//        DataAppContext dbContext = null;
+//        /// <summary>
+//        /// Target of our test.
+//        /// </summary>
+//        IRepository<User> repository = null;
+//        /// <summary>
+//        /// Object to be used by our target as an argument.
+//        /// </summary>
+//        User entity = null;
 
-        /// <summary>
-        /// Encapslates Arrange process segment in our tests.
-        /// </summary>
-        [TestInitialize]
-        public void Init()
-        {
-            this.dbContext = new DAL.DataAppContext();
-            this.factory = new DataApp.Core.Factories.RepoFactory();
-            this.repository = this.factory.CreateRepository<User>(this.dbContext);
-            this.entity = new User();
-            entity.Username = "New";
-            entity.Password = "New";
-        }
+//        /// <summary>
+//        /// Encapslates Arrange process segment in our tests.
+//        /// </summary>
+//        [TestInitialize]
+//        public void Init()
+//        {
+//            this.dbContext = new DAL.DataAppContext();
+//            this.factory = new DataApp.Core.Factories.RepoFactory();
+//            this.repository = this.factory.CreateRepository<User>(this.dbContext);
+//            this.entity = new User();
+//            entity.Username = "New";
+//            entity.Password = "New";
+//        }
 
-        #region Test CRUD
-        [TestMethod]
-        public void Add()
-        {
-            //arrange
-            //act
-            //done on Init()
-            //assert
-            Assert.IsTrue(this.repository.Add(this.entity));
-        }
+//        #region Test CRUD
+//        [TestMethod]
+//        public void Add()
+//        {
+//            //arrange
+//            //act
+//            //done on Init()
+//            //assert
+//            Assert.IsTrue(this.repository.Add(this.entity));
+//        }
 
-        [TestMethod]
-        public void Update()
-        {
-            //arrange
-            //act
-            this.repository.Add(this.entity);
-            this.entity.Username = "Update";
-            this.entity.Password = "Update";
+//        [TestMethod]
+//        public void Update()
+//        {
+//            //arrange
+//            //act
+//            this.repository.Add(this.entity);
+//            this.entity.Username = "Update";
+//            this.entity.Password = "Update";
 
-            //assert
-            Assert.IsTrue(this.repository.Update(this.entity));
-        }
+//            //assert
+//            Assert.IsTrue(this.repository.Update(this.entity));
+//        }
 
-        [TestMethod]
-        public void Delete()
-        {
-            //arrange
-            //act
-            this.repository.Add(this.entity);
-            //assert
-            Assert.IsTrue(this.repository.Update(this.entity));
-        }
+//        [TestMethod]
+//        public void Delete()
+//        {
+//            //arrange
+//            //act
+//            this.repository.Add(this.entity);
+//            //assert
+//            Assert.IsTrue(this.repository.Update(this.entity));
+//        }
 
-        [TestMethod]
-        public void Get()
-        {
-            //arrange
-            //act
-            var entity = this.repository.Get(p => p.Id > 0);
-            //assert
-            Assert.IsNotNull(entity);
-        }
+//        [TestMethod]
+//        public void Get()
+//        {
+//            //arrange
+//            //act
+//            var entity = this.repository.Get(p => p.Id > 0);
+//            //assert
+//            Assert.IsNotNull(entity);
+//        }
 
-        [TestMethod]
-        public void GetAll()
-        {
-            //arrange
-            //act
-            var users = this.repository.Get();
-            //assert
-            Assert.IsNotNull(users);
-        }
+//        [TestMethod]
+//        public void GetAll()
+//        {
+//            //arrange
+//            //act
+//            var users = this.repository.Get();
+//            //assert
+//            Assert.IsNotNull(users);
+//        }
 
-        [TestMethod]
-        public void GetAllWithFilter()
-        {
-            //arrange
-            //act
-            var users = this.repository.Get(u => u.Id > 1);
-            //assert
-            Assert.IsNotNull(users);
-        } 
-        #endregion
+//        [TestMethod]
+//        public void GetAllWithFilter()
+//        {
+//            //arrange
+//            //act
+//            var users = this.repository.Get(u => u.Id > 1);
+//            //assert
+//            Assert.IsNotNull(users);
+//        } 
+//        #endregion
 
-        #region Test CRUD ASYNC
-        //[TestMethod]
-        //public void GetAllAsync()
-        //{
-        //    var entities = this.repository.GetAllAsync().Result;
-        //    Assert.IsNotNull(entities);
-        //}
+//        #region Test CRUD ASYNC
+//        //[TestMethod]
+//        //public void GetAllAsync()
+//        //{
+//        //    var entities = this.repository.GetAllAsync().Result;
+//        //    Assert.IsNotNull(entities);
+//        //}
 
-        //[TestMethod]
-        //public void GetAllWithFilterAsync()
-        //{
-        //    //arrange
-        //    //act
-        //    var entities = this.repository.GetAllAsync(u => u.Id > 1).Result;
-        //    //assert
-        //    Assert.IsNotNull(entities);
-        //} 
-        #endregion
+//        //[TestMethod]
+//        //public void GetAllWithFilterAsync()
+//        //{
+//        //    //arrange
+//        //    //act
+//        //    var entities = this.repository.GetAllAsync(u => u.Id > 1).Result;
+//        //    //assert
+//        //    Assert.IsNotNull(entities);
+//        //} 
+//        #endregion
 
-    }
-}
+//    }
+//}
